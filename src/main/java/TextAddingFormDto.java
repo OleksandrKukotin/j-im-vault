b10c1acf1e3849cef64b8a550571d1699f3dd08b
Pawawudaf <oleksandr.kukotin@gmail.com>
2022-03-13 23:44:29 +0200

    Added DTO for adding text

---
+++ src/main/java/TextAddingFormDto.java
@@ -0,0 +1,21 @@
+public class TextAddingFormDto {
+
+    private String textAuthor;
+    private String textBody;
+
+    public String getTextAuthor() {
+        return textAuthor;
+    }
+
+    public void setTextAuthor(String textAuthor) {
+        this.textAuthor = textAuthor;
+    }
+
+    public String getTextBody() {
+        return textBody;
+    }
+
+    public void setTextBody(String textBody) {
+        this.textBody = textBody;
+    }
+}
